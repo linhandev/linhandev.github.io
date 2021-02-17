18ad5e562cc938aef5ed206f65291f2778e92355
lin <linhandev@qq.com>
2021-02-17 15:33:24 +0800

    modify

--- tools/build.sh
+++ tools/build.sh
@@ -6,6 +6,8 @@ git add *
 git commit -m "modify"
 git push
 
+curl -H 'Content-Type:text/plain' --data-binary @_site/overview/url.txt "http://data.zz.baidu.com/urls?site=https://linhandev.github.io&token=dpk3OOLJVr4bmarA"
+
 git checkout gh-page
 mv _site .site
 rm -rf *
@@ -17,5 +19,3 @@ git commit -m "add"
 git push
 
 git checkout main
-
-curl -H 'Content-Type:text/plain' --data-binary @_site/overview/url.txt "http://data.zz.baidu.com/urls?site=https://linhandev.github.io&token=dpk3OOLJVr4bmarA"
